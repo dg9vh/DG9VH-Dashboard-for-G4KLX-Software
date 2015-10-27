80c318724eddff698723433a37012d1e172ed161
dan-ai2m <ai2m@danporter.net>
2015-10-27 17:53:03 -0400

    Revert "Minor typo in comment"
    This reverts commit dee572f96f60b19d3b48644d8b55abee95fd9aa9.

--- ircddblocal.php
+++ ircddblocal.php
@@ -60,7 +60,7 @@ define("EMAILADDRESS", "dg9vh@darc.de");
 // set to true, if E-Mail-Address should be shown, false else
 define("SHOWEMAILADDRESS", true);
 
-// set to true, if Gateway Position-link should be shown, false else
+// set to true, if E-Mail-Address should be shown, false else
 define("SHOWGATEWAYPOSITION", true);
 
 // set to true, if APRS.fi-links should be shown, false else
